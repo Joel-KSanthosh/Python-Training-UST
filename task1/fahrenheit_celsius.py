e2f6b591a91fbc80b436bc94a2e0b1e320f390db
Joel-KSanthosh <joelksanthosh02@gmail.com>
2024-09-05 09:33:07 +0530

    remade fahrenheit_celcius using class and property added kelvin conversion

--- task1/fahrenheit_celsius.py
+++ task1/fahrenheit_celsius.py
@@ -9,8 +9,6 @@ class Celsius:
     def __str__(self) -> str:
         return f"Celcius Temperature = {self.celcius}"
     
-
-    
 class Kelvin:
     def __init__(self,fahrenheit) -> None:
         self.fahrenheit = fahrenheit
@@ -22,24 +20,23 @@ class Kelvin:
     def __str__(self) -> str:
         return f"Kelvin Temperature ={self.kelvin}"
     
-
 class Fahrenheit:
     def __init__(self,fahrenheit) -> None:
         self.fahrenheit = fahrenheit
     
+    @property
+    def calculate(self):
         print("[1]. Convert to Celcius\n[2]. Convert to Kelvin\n[3]. Exit")
         choice : int = int(input("Enter a choice : "))
         match choice:
-            case 1 : Celsius(self.fahrenheit)
-            case 2 : Kelvin(self.fahrenheit)
+            case 1 : print(Celsius(self.fahrenheit))
+            case 2 : print(Kelvin(self.fahrenheit))
             case 3 : exit()
-            case _ : return "Enter a valid choice !!\n"
-    
-    def __str__(self) -> str:
-        return self.__str__
+            case _ : print("Enter a valid choice !!\n")
+        
+        print()
     
 if __name__ == '__main__':
     while(1):
         value : int = int(input("Enter the fahrenheit temperature : "))
-        temperature_converter = Fahrenheit(value)
-        print(temperature_converter)
+        temperature_converter = Fahrenheit(value).calculate
